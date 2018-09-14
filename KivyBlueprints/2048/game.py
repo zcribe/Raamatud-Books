4b36df5d5e68c908a57410e6a8023c4389296250
Erlend Eelmets <debcf78e@opayq.com>
2018-09-14 10:31:17 +0300

    2048 midpoint

---
+++ KivyBlueprints/2048/game.py
@@ -0,0 +1,93 @@
+from kivy.app import App
+from kivy.graphics import BorderImage
+from kivy.uix.widget import Widget
+from kivy.graphics import Color, BorderImage
+from kivy.utils import get_color_from_hex
+from kivy.properties import ListProperty, NumericProperty
+
+
+spacing = 15
+colors = (
+    'EEE4DA', 'EDE0C8', 'F2B179', 'F59563',
+    'F67C5F', 'F65E3B', 'EDCF72', 'EDCC61',
+    'EDC850', 'EDC53F', 'EDC22E')
+
+tile_colors = {2 ** i: color for i, color in enumerate(colors, start=1)}
+
+
+def all_cells():
+    for x in range(4):
+        for y in range(4):
+            yield (x, y)
+
+
+class Tile(Widget):
+    font_size = NumericProperty(24)
+    number = NumericProperty(2) # Text shown on the tile
+    color = ListProperty(get_color_from_hex(tile_colors[2]))
+    number_color = ListProperty(get_color_from_hex('776E65'))
+
+    def __init__(self, number=2, **kwargs):
+        super(Tile, self).__init__(**kwargs)
+        self.font_size = 0.5 * self.width
+        self.number = number
+        self.update_colors()
+
+    def update_colors(self):
+        self.color = get_color_from_hex(
+            tile_colors[self.number])
+        if self.number > 4:
+            self.number_color = \
+                get_color_from_hex('F9F6F2')
+
+    def resize(self, pos, size):
+        self.pos = pos
+        self.size = size
+        self.font_size = 0.5 * self.width
+
+
+class Board(Widget):
+    b = None
+
+    def reset(self):
+        self.b = [[None for i in range(4)] for j in range(4)]
+
+    def __init__(self, **kwargs):
+        super(Board, self).__init__(**kwargs)
+        self.resize()
+
+    def resize(self, *args):
+        self.cell_size = (0.25 * (self.width - 5 * spacing), ) * 2
+        self.canvas.before.clear()
+        with self.canvas.before:
+            BorderImage(pos=self.pos, size=self.size, source='board.png')
+            Color(*get_color_from_hex('ccc0b4'))
+            for board_x, board_y in all_cells():
+                tile = self.b[board_x][board_y]
+                if tile:
+                    tile.resize(pos=self.cell_pos(board_x, board_y),
+                                size=self.cell_size)
+
+    def cell_pos(self, board_x, board_y):
+        return (self.x + board_x * (self.cell_size[0] + spacing) + spacing,
+                self.y + board_y * (self.cell_size[1] + spacing) + spacing)
+
+    def valid_cell(self, board_x, board_y):
+        return board_x >= 0 and board_y >= 0 and board_x <= 3 and board_y <= 3
+
+    def can_move(self, board_x, board_y):
+        return (self.valid_cell(board_x, board_y) and
+                self.b[board_x][board_y] is None)
+
+    on_pos = resize
+    on_size = resize
+
+
+class GameApp(App):
+    def on_start(self):
+        board = self.root.ids.board
+        board.reset()
+
+
+if __name__ == '__main__':
+    GameApp().run()
