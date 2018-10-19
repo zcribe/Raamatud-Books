d383eea1d46e7e8b37b39982562f69e41887ff5d
Erlend Eelmets <debcf78e@opayq.com>
2018-10-19 14:04:10 +0300

    Comments

--- kuroseRoss/Chapter 2/TCPServer.py
+++ kuroseRoss/Chapter 2/TCPServer.py
@@ -2,13 +2,17 @@ from socket import *
 
 server_port = 12000
 
+# Create socket
 server_socket = socket(AF_INET, SOCK_STREAM)
+# Bind welcoming socket to port
 server_socket.bind(("", server_port))
+# Listen for TCP requests. 1 max queued connection.
 server_socket.listen(1)
 
 print("Ready to recieve.")
 
 while True:
+    # accept the connection, complete the handshake. Create TCP connenction.
     connection_socket, addr = server_socket.accept()
     sentence = connection_socket.recv(1024).decode()
     response_sentence = sentence.upper()
