dca19aea598d70e64664ab4508b83fdaa7aa0eb0
Erlend Eelmets <debcf78e@opayq.com>
2018-10-19 13:39:09 +0300

    Comments

--- kuroseRoss/Chapter 2/UDPClient.py
+++ kuroseRoss/Chapter 2/UDPClient.py
@@ -1,8 +1,11 @@
 from socket import *
 serverName = "127.0.0.1"
 serverPort = 12000
+# Create client socket. AF_INET = ipv4. SOCK_DGRAM = UDP socket
 clientSocket = socket(AF_INET, SOCK_DGRAM)
+
 message = input("Input lowercase sentence:")
+# Encode message into bytes and send to specific target location
 clientSocket.sendto(message.encode(), (serverName, serverPort))
 modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
 print(modifiedMessage.decode())
