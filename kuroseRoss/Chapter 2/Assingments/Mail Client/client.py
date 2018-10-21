128bcb7eb14177c1e7e64631ef800c5f6a117ca5
Erlend Eelmets <debcf78e@opayq.com>
2018-10-21 14:34:33 +0300

    Mail client initial

---
+++ kuroseRoss/Chapter 2/Assingments/Mail Client/client.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from socket import socket, AF_INET, SOCK_STREAM
+
+
+class Main:
+    def __init__(self):
+        self.port = 
+
+    def run(self):
+        pass
+
+    def send_mail(self):
+        pass
